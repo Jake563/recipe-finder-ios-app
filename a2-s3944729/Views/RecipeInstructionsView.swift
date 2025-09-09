add09721892a448f38be5ed59f294d9618fb4c29
Jake563 <s3944729@student.rmit.edu.au>
2025-09-09 18:31:31 +1000

    Created fundamental layout of RecipeInstructionsView (no functionality yet)

--- a2-s3944729/Views/RecipeInstructionsView.swift
+++ a2-s3944729/Views/RecipeInstructionsView.swift
@@ -10,10 +10,85 @@
 import SwiftUI
 
 struct RecipeInstructionsView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var currentStep: String = "Preheat oven for 8 minutes"
+    @State private var currentStepIndex: Int = 0
+    
     let recipe: Recipe
     
+    private func nextStep() {
+
+    }
+    
+    private func prevStep() {
+
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                HStack {
+                    Text(currentStep)
+                    Spacer()
+                }
+                Spacer()
+                VStack {
+                    HStack {
+                        Image(systemName: "clock")
+                        Text("07:00")
+                    }
+                    Button(action: {
+                        nextStep()
+                    }) {
+                        Text("Start Timer")
+                    }
+                    .font(.title)
+                    .buttonStyle(PrimaryButtonStyle())
+                }
+                Spacer()
+                HStack {
+                    Button(action: {
+                        prevStep()
+                    }) {
+                        Text("< Prev")
+                    }
+                    .font(.title)
+                    .buttonStyle(PrimaryButtonStyle())
+                    Spacer()
+                    Button(action: {
+                        nextStep()
+                    }) {
+                        Text("Next >")
+                    }
+                    .font(.title)
+                    .buttonStyle(PrimaryButtonStyle())
+                }
+            }
+            .padding()
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "arrow.left")
+                            .foregroundColor(.black)
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("Step 1")
+                        .font(.title)
+                }
+                ToolbarItem() {
+                    Button(action: {
+                        print("Help")
+                    }) {
+                        Image(systemName: "questionmark.circle")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -25,6 +100,8 @@ struct RecipeInstructionsView: View {
             RequiredIngredient(name: "Egg", quantity: 50, quantityMassUnit: nil),
             RequiredIngredient(name: "Milk", quantity: 20, quantityMassUnit: "mL")
         ],
-        instructions: []
+        instructions: [
+            "Preheat stove top for 10 minutes."
+        ]
     ))
 }
