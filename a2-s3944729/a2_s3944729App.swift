2aebb0fcb4e4d7f601780c023e1b85347266f51f
Jake563 <s3944729@student.rmit.edu.au>
2025-09-18 18:28:11 +1000

    Setup Firebase

--- a2-s3944729/a2_s3944729App.swift
+++ a2-s3944729/a2_s3944729App.swift
@@ -7,9 +7,23 @@
 
 import SwiftData
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 
 @main
 struct a2_s3944729App: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
