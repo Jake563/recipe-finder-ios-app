1e27b734a55d91164effdf1a16806b14f11522a9
Jake563 <s3944729@student.rmit.edu.au>
2025-09-20 14:09:02 +1000

    Deleted temporary file

--- a2-s3944729/Services/MyPlayground.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-print("running")
-Task {
-    let stepClarifcation = await AiService.getRecipeStepClarification(instruction: Instruction(instruction: "Mix all ingredients in a bowl", timer: 0))
-    print(stepClarifcation)
-}
