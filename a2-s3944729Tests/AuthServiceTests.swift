7a2de95d8a5060475a10b19a0d54e25aa7e5cc29
Jake563 <s3944729@student.rmit.edu.au>
2025-09-22 18:40:42 +1000

    Enhanced test cases

--- a2-s3944729Tests/AuthServiceTests.swift
+++ a2-s3944729Tests/AuthServiceTests.swift
@@ -12,25 +12,24 @@ import Foundation
 
 private class MockFirebaseAuthService: FirebaseAuthServiceProtocol {
     var signedInUserID: String?
-    var shouldThrowError = false
-    var errorToThrow: Error? = NSError(domain: "domain", code: 1)
+    var errorToThrow: Error?
     
     func createUser(withEmail: String, password: String) async throws -> String? {
-        if shouldThrowError {
+        if errorToThrow != nil {
             throw errorToThrow!
         }
         return signedInUserID
     }
     
     func signIn(email: String, password: String) async throws -> String? {
-        if shouldThrowError {
+        if errorToThrow != nil {
             throw errorToThrow!
         }
         return signedInUserID
     }
     
     func signOut(withEmail: String, password: String) async throws {
-        if shouldThrowError {
+        if errorToThrow != nil {
             throw errorToThrow!
         }
     }
@@ -41,54 +40,48 @@ private class MockFirebaseAuthService: FirebaseAuthServiceProtocol {
 }
 
 struct AuthServiceTests {
-    @Test func testSignUp_networkFailure_returnsUnknownError() async throws {
+    @Test func testSignUp_unknownError_throwsUnknownError() async throws {
         let mockFirebaseAuthService = MockFirebaseAuthService()
-        mockFirebaseAuthService.shouldThrowError = true
+        mockFirebaseAuthService.errorToThrow = NSError(domain: "domain", code: -123)
         let authService = AuthService(firebaseAuthService: mockFirebaseAuthService)
 
         await #expect(throws: AuthService.AuthError.unknownError) {
-            _ = try await authService.signUp(email: "test@test.com", password: "password")
+            try await authService.signUp(email: "test@test.com", password: "password")
         }
     }
     
-    @Test func testSignUp_weakPassword_returnsWeakPasswordError() async throws {
+    @Test func testSignUp_weakPassword_throwsWeakPasswordError() async throws {
         let WEAK_PASSWORD_CODE = 17026
         let mockFirebaseAuthService = MockFirebaseAuthService()
-        mockFirebaseAuthService.shouldThrowError = true
         mockFirebaseAuthService.errorToThrow = NSError(domain: "FIRAuthErrorDomain", code: WEAK_PASSWORD_CODE)
         let authService = AuthService(firebaseAuthService: mockFirebaseAuthService)
 
         await #expect(throws: AuthService.AuthError.weakPassword) {
-            _ = try await authService.signUp(email: "test@test.com", password: "123")
+            try await authService.signUp(email: "test@test.com", password: "123")
         }
     }
     
     @Test func testSignIn_accountDoesNotExist_throwsAccountNotFoundError() async throws {
         let ACCOUNT_NOT_FOUND_ERROR_CODE = 17011
         let mockFirebaseAuthService = MockFirebaseAuthService()
-        mockFirebaseAuthService.shouldThrowError = true
         mockFirebaseAuthService.errorToThrow = NSError(domain: "FIRAuthErrorDomain", code: ACCOUNT_NOT_FOUND_ERROR_CODE)
         let authService = AuthService(firebaseAuthService: mockFirebaseAuthService)
 
         await #expect(throws: AuthService.AuthError.accountNotFound) {
-            _ = try await authService.signIn(email: "non-exist@test.com", password: "123")
+            try await authService.signIn(email: "non-exist@test.com", password: "123")
         }
     }
     
-    @Test func testSignIn_networkFailure_returnsUnknownError() async throws {
-        struct GenericError: Error {}
-        
+    @Test func testSignIn_signInSucceeds_throwsNoError() async throws {
         let mockFirebaseAuthService = MockFirebaseAuthService()
-        mockFirebaseAuthService.shouldThrowError = true
-        mockFirebaseAuthService.errorToThrow = GenericError()
         let authService = AuthService(firebaseAuthService: mockFirebaseAuthService)
 
-        await #expect(throws: AuthService.AuthError.unknownError) {
-            _ = try await authService.signIn(email: "test@test.com", password: "123")
-        }
+        try await authService.signIn(email: "test@test.com", password: "chsbhabdshasdjn")
+        
+        // This test will pass if signIn throws no error, hence why there is no #expect here.
     }
     
-    @Test func testIsLoggedIn_userLoggedIn_returnsTrue() async throws {
+    @Test func testIsLoggedIn_nonNilUserId_returnsTrue() async throws {
         let mockFirebaseAuthService = MockFirebaseAuthService()
         mockFirebaseAuthService.signedInUserID = "signed-in-user-id"
         let authService = AuthService(firebaseAuthService: mockFirebaseAuthService)
@@ -98,7 +91,7 @@ struct AuthServiceTests {
         #expect(loggedIn == true)
     }
     
-    @Test func testIsLoggedIn_userNotLoggedIn_returnsFalse() async throws {
+    @Test func testIsLoggedIn_nilUserId_returnsFalse() async throws {
         let mockFirebaseAuthService = MockFirebaseAuthService()
         mockFirebaseAuthService.signedInUserID = nil
         let authService = AuthService(firebaseAuthService: mockFirebaseAuthService)
